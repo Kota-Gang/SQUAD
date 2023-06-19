2bc5c62231d64695c615d251d13ef29943eb0fb5
mayank kumar ratre <mayankratre10@gmail.com>
2023-06-19 16:26:19 +0530

    Delete Routes For Sign In and Sign Up and Home Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import SignIn from './Pages/Authenctication/SignIn/SignIn'
-import Signup from './Pages/Authenctication/SignUp/Signup'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './Pages/Home/Home'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
-        <Route path="/signin" element={<SignIn/>}></Route>
-        <Route path="/signup" element={<Signup/>}></Route>
       </Routes>
     </BrowserRouter>
   )
