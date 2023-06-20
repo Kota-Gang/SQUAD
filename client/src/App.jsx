e9c824f78e6daf65695305a41fcc585732e33dfc
mayank kumar ratre <mayankratre10@gmail.com>
2023-06-20 16:39:17 +0530

    Add Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './Pages/Home/Home'
 import EmailVerification from "./Pages/Authenctication/emailVerification"
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/squadaccountemailverification" element={<EmailVerification/>}></Route>
         <Route path="/meet?id" element={<Home/>}></Route>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
